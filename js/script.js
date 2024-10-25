a848b232caafa75cf922398a84c5f6a52169e822
Mohiduzzaman Mahin <mahin.swe.14@gmail.com>
2024-10-25 22:02:08 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   loop:true,
   autoplay: {
-    delay: 9500,
+    delay: 2500,
     disableOnInteraction: false,
   },
   pagination: {
